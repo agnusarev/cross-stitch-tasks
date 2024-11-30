a989d89964d8a7f402c24861b6e6dceb8b648c02
Alexander Gnusarev <alexander.gnussarev@gmail.com>
2024-11-30 17:52:20 +0600

    check test

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,11 +1,12 @@
-from typing import Any, TYPE_CHECKING
+from typing import Any
 
 from tests.base_test import BaseTest
 
 
 class TestCommon(BaseTest):
 
-    def test_app(self, crud: Any) -> None:
-        if TYPE_CHECKING:
-            print(crud)
-        assert True
+    def test_insert(self, crud: Any) -> None:
+        crud.insert("types_of_base", {"type_of_base": "лен"})
+        crud.insert("types_of_base", {"type_of_base": "дерево"})
+
+        assert crud.get_actual_table("types_of_base").shape[1] == 2
