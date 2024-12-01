bc338b27b4543a39dacce1e55c13210ae6a9ac23
Alexander Gnusarev <alexander.gnussarev@gmail.com>
2024-12-01 12:19:43 +0600

    поправил миграцию чтобы были auto joins

--- src/cross_stitch_tasks/api/models/cross_stitch.py
+++ src/cross_stitch_tasks/api/models/cross_stitch.py
@@ -1,5 +1,5 @@
-from sqlalchemy import BOOLEAN, INTEGER, VARCHAR, Column, ForeignKey
-# from sqlalchemy.orm import relationship
+from sqlalchemy import BOOLEAN, INTEGER, VARCHAR, Column, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 from cross_stitch_tasks.api.models.base_model import BaseModel
 
@@ -14,19 +14,19 @@ from cross_stitch_tasks.api.models.base_model import BaseModel
 
 class TypeOfBase(BaseModel):
     __tablename__ = "types_of_base"
-    id = Column(INTEGER(), primary_key=True, autoincrement=True, comment="Идентификатор вида основы")
+    id = Column(INTEGER(), primary_key=True, unique=True, comment="Идентификатор вида основы")
     type_of_base = Column(VARCHAR(100), nullable=False, comment="Тип основы")
 
 
 class TypeOfImage(BaseModel):
     __tablename__ = "types_of_image"
-    id = Column(INTEGER(), primary_key=True, autoincrement=True, comment="Идентификатор типа изображения")
+    id = Column(INTEGER(), primary_key=True, unique=True, comment="Идентификатор типа изображения")
     type_of_image = Column(VARCHAR(100), nullable=False, comment="Тип изображения")
 
 
 class Jobs(BaseModel):
     __tablename__ = "jobs"
-    id = Column(INTEGER(), primary_key=True, autoincrement=True, comment="Идентификатор работы")
+    id = Column(INTEGER(), primary_key=True, unique=True, comment="Идентификатор работы")
     length_in_cm = Column(INTEGER(), nullable=False, comment="Длина работы в сантиметрах")
     width_in_cm = Column(INTEGER(), nullable=False, comment="Ширина работы в сантиметрах")
     length_in_crosses = Column(INTEGER(), nullable=False, comment="Длина работы в крестиках")
@@ -39,18 +39,40 @@ class Jobs(BaseModel):
     number_of_blends = Column(INTEGER(), nullable=False, comment="Количество блендов в работе")
     is_active = Column(BOOLEAN(), nullable=False, comment="Флаг активности работы")  # type: ignore
 
-    type_of_base_id = Column(INTEGER(), ForeignKey("types_of_base.id"), comment="Вид основы")
-    type_of_image_id = Column(INTEGER(), ForeignKey("types_of_image.id"), comment="Тип изображения")
+    type_of_base_id = Column(
+        INTEGER(), ForeignKey("types_of_base.id"), unique=True, primary_key=True, comment="Вид основы"
+    )
+    type_of_image_id = Column(
+        INTEGER(), ForeignKey("types_of_image.id"), unique=True, primary_key=True, comment="Тип изображения"
+    )
     # user_id = Column(INTEGER(), ForeignKey("users.id"), comment="Пользователь")
 
-    # type_of_base = relationship("TypeOfBase", foreign_keys="[TypeOfBase.id]")
+    type_of_base = relationship(
+        "TypeOfBase",
+        secondary=Table(
+            "jobs_type_of_base_assocoations",
+            BaseModel.metadata,
+            Column("jobs_type_of_base_id", ForeignKey("jobs.type_of_base_id")),
+            Column("types_of_base_id", ForeignKey("types_of_base.id")),
+        ),
+    )
     # type_of_image = relationship("TypeOfImage", foreign_keys="[TypeOfImage.id]")
+
+    type_of_image = relationship(
+        "TypeOfImage",
+        secondary=Table(
+            "jobs_type_of_image_assocoations",
+            BaseModel.metadata,
+            Column("jobs_type_of_image_id", ForeignKey("jobs.type_of_image_id")),
+            Column("type_of_image_id", ForeignKey("types_of_image.id")),
+        ),
+    )
 
 
 class Processes(BaseModel):
     __tablename__ = "processes"
-    id = Column(INTEGER(), primary_key=True, autoincrement=True, comment="Идентификатор процесса")
-    job_id = Column(INTEGER(), ForeignKey("jobs.id"), comment="Идентификатор работы")
+    id = Column(INTEGER(), primary_key=True, unique=True, comment="Идентификатор процесса")
+    job_id = Column(INTEGER(), ForeignKey("jobs.id"), primary_key=True, unique=True, comment="Идентификатор работы")
     number_of_crosses = Column(INTEGER(), nullable=True, comment="Текущее количество вышитых крестиков")
     number_of_half_crosses = Column(INTEGER(), nullable=True, comment="Текущее количество вышитых полукрестиков")
     number_of_backstitch = Column(INTEGER(), nullable=True, comment="Текущее количество вышитого бэкстича")
