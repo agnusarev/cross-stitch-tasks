20695d24ee914c18d15b1f4fb915a87dd1ccf695
Alexander Gnusarev <alexander.gnussarev@gmail.com>
2024-12-07 10:59:22 +0600

    reformatting tests

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,23 +1,36 @@
 import pandas as pd
+from flask import Flask
+from sqlalchemy import select
 
 from cross_stitch_tasks.api.data_base_helper import DataBaseHelper
+from cross_stitch_tasks.api.models import TypeOfBase, TypeOfImage
 from tests.base_test import BaseTest
 
 
 class TestDataBaseHelper(BaseTest):
 
-    def test_insert_types_of_base(self, crud: "DataBaseHelper") -> None:
-        expected = pd.DataFrame.from_dict({"id": [1, 2], "type_of_base": ["пластик", "дерево"]})
-        crud.insert("types_of_base", {"type_of_base": "пластик"})
-        crud.insert("types_of_base", {"type_of_base": "дерево"})
-        test_df = crud.get_actual_table("types_of_base")
+    def test_insert(self, crud: "DataBaseHelper") -> None:
+        type_of_base_dict = {"type_of_base": "пластик"}
+        crud.insert("types_of_base", type_of_base_dict)
+        _type_of_base = crud.db.session.execute(select(TypeOfBase.type_of_base)).scalar_one()
+        assert _type_of_base == type_of_base_dict["type_of_base"]
 
-        pd.testing.assert_frame_equal(expected, test_df)
+        type_of_image_dict = {"type_of_image": "пейзаж"}
+        crud.insert("types_of_image", type_of_image_dict)
+        _type_of_image = crud.db.session.execute(select(TypeOfImage.type_of_image)).scalar_one()
+        assert _type_of_image == type_of_image_dict["type_of_image"]
 
-    def test_insert_types_of_image(self, crud: "DataBaseHelper") -> None:
-        expected = pd.DataFrame.from_dict({"id": [1, 2], "type_of_image": ["пейзаж", "цветы"]})
-        crud.insert("types_of_image", {"type_of_image": "пейзаж"})
-        crud.insert("types_of_image", {"type_of_image": "цветы"})
+    def test_get_actual_table(self, crud: "DataBaseHelper") -> None:
+        expected = pd.DataFrame.from_dict({"id": [1], "type_of_image": ["пейзаж"]})
         test_df = crud.get_actual_table("types_of_image")
 
         pd.testing.assert_frame_equal(expected, test_df)
+
+
+class TestApi(BaseTest):
+    def test_forms(self, get_client_: "Flask") -> None:
+        response = get_client_.get("/type")
+        assert response.status_code == 200  # type: ignore
+
+        response = get_client_.get("/image")
+        assert response.status_code == 200  # type: ignore
